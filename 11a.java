ccd9c6b0c0c3290488350cc88da588a780ad59c4
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-12-25 18:06:04 +0530

    Create 11a.java

---
+++ 11a.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class prog
+{
+	public static void main(String args[])
+	{
+		String s = ""; 
+		int count = 0;
+
+		System.out.println("Enter your string:"); 
+		Scanner sc = new Scanner(System.in);
+		s += sc.nextLine();
+
+		s=s.toUpperCase();
+		//System.out.println(s);
+		s=s.replaceAll(" ", "");
+		//System.out.println(s);
+
+		for(int i=0;i<s.length()-1;i++) 
+			if(s.charAt(i) == s.charAt(i+1))
+				count+=1;
+		
+		System.out.println(count);
+
+	}
+}
