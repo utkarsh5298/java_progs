259bc160b645ef33c1b1a31f7d9c2e556b3af915
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 14:23:01 +0530

    Update 11b.java

--- 11b.java
+++ 11b.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -6,32 +5,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-{
-	PrintWriter out = response.getWriter();
-	response.setContentType("text/html");
+@WebServlet("/Q11b")
+public class Q11b extends HttpServlet {
 	
-	try
-		{
-Class.forName("com.mysql.jdbc.Driver");
-Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root","");
-Statement st = (Statement) conn.createStatement();
-ResultSet rs=st.executeQuery("select * from subjects");
-	while(rs.next())
-	{
-	int fid=rs.getInt("FacID");
-String sub=rs.getString("SubName");
-	out.println("FacID "+fid+" SubName "+sub);
-	out.println("<br>");
-	}			
-PreparedStatement pst=(PreparedStatement) conn.prepareStatement("update subjects set SubName='java' where SubID=1 and FacID=1");
-int n = pst.executeUpdate();
-out.println("Number of Rows Updated "+n );
-out.println("<br>");	
-}
-catch(Exception e)
-{
-out.println(e);
-}
+	private static final long serialVersionUID = 1L;
+
+    public Q11b() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter out = response.getWriter();
+    	response.setContentType("text/html");
+    	
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root","");
+    		Statement st = con.createStatement();
+    		ResultSet rs=st.executeQuery("select * from subjects");
+    		
+    		while(rs.next()) {
+    			int fid=rs.getInt("FacID");
+    			String sub=rs.getString("SubName");
+    			out.println("FacID "+fid+" SubName "+sub);
+    			out.println("<br>");
+    		}			
+    		PreparedStatement ps= con.prepareStatement("update subjects set SubName='java' where FacID=1");
+    		int n = ps.executeUpdate();
+    		out.println("Number of Rows Updated "+n );
+    		out.println("<br>");	
+    	}
+    	catch(Exception e) {
+    		out.println(e);
+    	}
+	}
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
 }
