b72efd8933bf7fe3ec1906de971ebe54c93c855b
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 13:23:49 +0530

    Update 14b.java

--- 14b.java
+++ 14b.java
@@ -1,14 +1,35 @@
-"select * from student where cgpa<9";
+import java.sql.*;
+import java.util.*;
+public class prog{
 
-Statement st1=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-String query1="select name,cgpa from student";
-ResultSet rs1=st1.executeQuery(query1);
-    while(rs1.next())
-	{
-	    	if(rs1.getString("name").equals("john"))
-	    	{
-	                	break;
-	    	}
+	public static void main(String[] args) {
+		Connection con=null;
+		Scanner input=new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
+			Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			con.setAutoCommit(false);
+			ResultSet rs;
+			rs=stmt.executeQuery("select * from college");
+			Savepoint sp1=con.setSavepoint();
+			while(rs.next()) {
+				if(rs.getDouble("cgpa")<9.0)
+				{
+					System.out.println(" Students with less than 9 cgpa is "+rs.getString("name"));
+				}
+				if(rs.getString("Name").equals("John")) {
+					rs.updateDouble(4, 9.4);
+					rs.updateRow();
+					}
+				}
+			}
+		}
+		}
+		catch(ClassNotFoundException e) {e.printStackTrace();}
+		catch(BatchUpdateException e) {e.printStackTrace();}
+		catch(SQLException e) {e.printStackTrace();}
+		catch (Exception e) {e.printStackTrace();}
+		
 	}
-rs1.updateDouble("cgpa", 9.4);
-rs1.updateRow(); 
+}
