42035a3ec9f0e8798d46491e044579354f5f6716
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 13:42:22 +0530

    Update 3b.java

--- 3b.java
+++ 3b.java
@@ -5,40 +5,38 @@
 <title>Police Station </title>
 </head>
 <body>
-
-<form action = "Initials" method = post >
-Enter Your Name  : <input type = "text" name = "name" > <br>
-<button type = "Submit"  > Submit </button>
-<button type  = "Reset"> Cancel </button>
-
+    <form action = "Initials" method = "post" >
+    Enter Your Name  : <input type = "text" name = "name" > <br>
+    <button type = "Submit"> Submit </button>
+    <button type = "Reset"> Cancel </button>
 </form> 
 </body>
 </html>
 
 //Servlet
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 @WebServlet("/Initials")
 public class Initials extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-	public Initials() { super(); }
-	public String initials(String n)
-	{
-		String in = Character.toString(n.charAt(0));
-	for (int i=0; i<n.length(); i++)
-	if (n.charAt(i)==' ' && i+1 < n.length() &&  n.charAt(i+1)!=' ' )
-		in+=n.charAt(i+1);
-			
+	
+	public Initials() { 
+		super(); 
+	}
+	
+	public String initials(String name) {
+		String in = Character.toString(name.charAt(0));
+		for (int i = 1; i < name.length(); i++) 
+			if (name.charAt(i) == ' ')
+				in+=name.charAt(i + 1); 
 		return in.toUpperCase();
 	}
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String n = request.getParameter("name");
@@ -46,7 +44,8 @@ public class Initials extends HttpServlet {
 		out.println(in);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{  doGet(request, response);  }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
