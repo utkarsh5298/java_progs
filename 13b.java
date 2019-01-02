ceaa15534980d574227aea505939f9781d8c5886
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 14:29:23 +0530

    Update 13b.java

--- 13b.java
+++ 13b.java
@@ -1,17 +1,28 @@
 //create cookies
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+@WebServlet("/SetCookies")
+public class SetCookies extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+    public SetCookies() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		response.setContentType("text/html");
 		PrintWriter pw=response.getWriter();
-		for(int i=1;i<4;i++)
+		for(int i=0;i<3;i++)
 		{
 			Cookie mycookie=new Cookie("session"+i,"value"+i);
 			response.addCookie(mycookie);
@@ -20,19 +31,35 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 			response.addCookie(mycookie);
 		}
 		pw.println("cookie created");
+		RequestDispatcher rd=request.getRequestDispatcher("ReadCookie");
+    		rd.include(request, response);
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 
+}
 //read cookies
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+@WebServlet("/ReadCookie")
+public class ReadCookie extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+    public ReadCookie() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter pw=response.getWriter();
 		
@@ -43,11 +70,16 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 		if(cookies==null)
 			pw.println("No Cookies <br>");
 		else {
-		for(int i=0;i<cookies.length;i++)
-		{
-			name=cookies[i].getName();
-			value=cookies[i].getValue();
-pw.println("<tr> <td>"+ name +"</td>  <td>"+value+"</td> </tr><br>");
+			for(int i=0;i<cookies.length;i++) {
+				name=cookies[i].getName();
+				value=cookies[i].getValue();
+				pw.println("<tr> <td>"+ name +"</td>  <td>"+value+"</td> </tr><br>");
+			}
+		}
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
-	}
+
 }
