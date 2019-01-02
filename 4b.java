2250fc3ebec170b58af554371fbc009515800d21
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 13:52:22 +0530

    Update 4b.java

--- 4b.java
+++ 4b.java
@@ -1,38 +1,40 @@
 //register.html
-<form action = "welcome.jsp" method = post>
-Enter user name: <input type="text" name="user"><br>
-Enter password: <input type="text" name="pass"><br>
-
-<button type = "submit"> Register </button>
-</form>
-
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action = "welcome.jsp" method = "post">
+		Enter user name: <input type="text" name="user"><br>
+		Enter password: <input type="password" name="pass"><br>
+		<button type = "submit"> Register </button>
+	</form>
+</body>
+</html>
 
 //welcome.jsp
 <%
-String name =request.getParameter("user");
-String password=request.getParameter("pass");
-if(name.equals("admin") && password.equals("1234"))
-{
-	out.println("Welcome "+ name);
-	HttpSession s = request.getSession();
-	session.setAttribute("user",name);
-    request.getRequestDispatcher("sessiontrace.jsp").forward(request,response);
-}
-else
-out.println("Not Valid! ");
+	String name =request.getParameter("user");
+	String password=request.getParameter("pass");
+	if(name.equals("admin") && password.equals("1234")) {
+		session.setAttribute("user",name);
+    	request.getRequestDispatcher("sessiontrace.jsp").forward(request,response);
+	}
+	else {
+		out.println("Not Valid!");	
+	}
 %>
 
 //sessiontrace.jsp
 <%
-HttpSession s=request.getSession(false);
-if(session!=null)
-{
-	String name=(String)session.getAttribute("user");
-	out.println("Hello "+ name +" Welcome");
-}
-else
-{
-	out.println("Please login first");
-	request.getRequestDispatcher("register.html").include(request,response);
-}
+	if(session!=null) {
+		String name=(String)session.getAttribute("user");
+		out.println("Hello "+ name +", Welcome");
+	}
+	else {
+		out.println("Please login first");
+		request.getRequestDispatcher("register.html").include(request,response);
+	}
 %>
