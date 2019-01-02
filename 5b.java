fcb25e1f2c831914bd6a735fd087aadf3de2de4c
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 13:54:37 +0530

    Update 5b.java

--- 5b.java
+++ 5b.java
@@ -1,24 +1,40 @@
 //main.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
 <body>
-<form action = "ticket.jsp" method = post >
-Enter Name : <input type ="text" name = "name" > <br>
-Enter Age :  <input type ="text" name = "age" > <br>
-<button type = "Submit"> Submit </button>
-</form> 
+	<form action = "ticket.jsp" method = post >
+		Enter Name : <input type ="text" name = "name" > <br>
+		Enter Age :  <input type ="text" name = "age" > <br>
+		<button type = "Submit"> Submit </button>
+	</form> 
 </body>
+</html>
 
 
 //ticket.jsp
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
 <body>
 <%
-String n = request.getParameter("name");
-int a = Integer.parseInt(request.getParameter("age"));
-out.println("Hello, " + n + " " + a + " "); 
-if(a>62)
-	out.println("Your movie ticket price is Rs 7.00");
-else if(a<10)
-	out.println("Your movie ticket price is Rs 5.00");
-else
-	out.println("Your movie ticket price is Rs 9.50");
+	String n = request.getParameter("name");
+	int a = Integer.parseInt(request.getParameter("age"));
+	out.println("Hello: " + n + " age: " + a + "\n"); 
+	if(a>62)
+		out.println("Your movie ticket price is Rs 50.00");
+	else if(a<10)
+		out.println("Your movie ticket price is Rs 30.00");
+	else
+		out.println("Your movie ticket price is Rs 80.00");
 %>
 </body>
+</html>
