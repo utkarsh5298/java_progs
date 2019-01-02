d847488de34e502d242582067c108b4feb39fa86
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 12:55:28 +0530

    Update 10b.java

--- 10b.java
+++ 10b.java
@@ -9,6 +9,13 @@ Enter password:<input type="text" name="password"><br>
 </body>
 
 //cookie read
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
  {
