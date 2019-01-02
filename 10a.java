d4b621d7bef6ef4ead2aface25c838383ef85aea
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 18:20:47 +0530

    Update 10a.java

--- 10a.java
+++ 10a.java
@@ -21,20 +21,12 @@ public class Pythagorean
 	
 	public void triplets(int limit)
 	{
-	    // triplet:  a^2 + b^2 = c^2
 	    int a, b, c=0;
-	 
-	    //  loop from 2 to max_limitit
 	    int m = 2;
-	 
-	    // Limiting c would limit all a, b and c
 	    while (c < limit)
 	    {
-	        // now loop on j from 1 to i-1
 	        for (int n = 1; n < m; ++n)
 	        {
-	            // Evaluate and print triplets using
-	            // the relation between a, b and c
 	            a = m*m - n*n;
 	            b = 2*m*n;
 	            c = m*m + n*n;
