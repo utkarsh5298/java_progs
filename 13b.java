857c6607e1453afcb70a2c10ac62a097cc7e6d27
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 12:58:40 +0530

    Update 13b.java

--- 13b.java
+++ 13b.java
@@ -1,4 +1,11 @@
 //create cookies
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
@@ -16,6 +23,14 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 	}
 
 //read cookies
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		response.setContentType("text/html");
