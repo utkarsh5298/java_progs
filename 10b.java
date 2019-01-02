eb9592bf694be96bd529c5c7a630fcb45aae608f
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-01-02 14:19:57 +0530

    Update 10b.java

--- 10b.java
+++ 10b.java
@@ -1,72 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
 <body>
-<form action="Cookieread" method="post">
-Enter name:<input type="text" name="name"><br>
-Enter panid :<input type="text" name="panid"><br>
-Enter password:<input type="text" name="password"><br>
-<button type="submit">Submit</button><br>
-<button type="reset">Cancel</button>
-</form> 
+	<form action="Cookieread" method="post">
+		Enter name:<input type="text" name="name"><br>
+		Enter panid :<input type="text" name="panid"><br>
+		Enter password:<input type="text" name="password"><br>
+		<button type="submit">Submit</button><br>
+		<button type="reset">Cancel</button>
+	</form> 
 </body>
+</html>
 
-//cookie read
+//Cookieread.java
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
- {
-	Cookie mycookie=new Cookie("panid","-999");
-	mycookie.setMaxAge(3600);
-	response.addCookie(mycookie);
+@WebServlet("/Cookieread")
+public class Cookieread extends HttpServlet {
 
-RequestDispatcher rd=request.getRequestDispatcher("Cookiewrite");
-	rd.forward(request, response);
+	private static final long serialVersionUID = 1L;
+
+    public Cookieread() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String panid=request.getParameter("panid");
+    	Cookie mycookie=new Cookie("panid",panid);
+    	mycookie.setMaxAge(3600);
+    	response.addCookie(mycookie);
+    	RequestDispatcher rd=request.getRequestDispatcher("Cookiewrite");
+    	rd.forward(request, response);
+
+   }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
 
 
-//cookie write
+//Cookiewrite.java
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-{
-				response.setContentType("text/html");
-PrintWriter out=response.getWriter();
+@WebServlet("/Cookiewrite")
+public class Cookiewrite extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+    
+    public Cookiewrite() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
 		Cookie[] cookies=request.getCookies();
 		String name="";
 		String value="";
 		String n1 = request.getParameter("name");
 		String pid = request.getParameter("panid");
 		int c=0;
+		
+		for(int i=0; i<cookies.length; i++) {
+			name = cookies[i].getName();
+			value = cookies[i].getValue();
+			if(name.equals("admin") && value.equals(pid)) {
+				out.println("Welcome Back"+ n1);
+				c=1;
+				break;	
+			}
+		}
+		
+		if(c==0) {
+			Cookie mycookie = new Cookie("admin",pid);
+			mycookie.setMaxAge(3600);
+			response.addCookie(mycookie);
+			out.println("Welcome");
+		}
+		out.close();
+    }
 
-for(int i=0; i<cookies.length; i++)
-{
-	name = cookies[i].getName();
-	value = cookies[i].getValue();
-	if(name.equals("admin") && value.equals(pid))
-	{
-		out.println("Welcome Back"+ n1);
-		c=1;
-		break;	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
-}
 
-if(c==0)
-{
-	Cookie mycookie = new Cookie("admin",pid);
-	mycookie.setMaxAge(3600);
-	response.addCookie(mycookie);
-	out.println("welcome");
-}
-out.close();
 }
